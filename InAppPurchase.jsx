b05fa81dacf034cdca5e64ee107f5a00b11a158c
Etienne Lacoursiere <lacoursiere.etienne@gmail.com>
2023-04-12 13:07:38 -0400

    Add comment

--- InAppPurchase.jsx
+++ InAppPurchase.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Text } from 'react-native'
+// Just importing the module here breaks the web app even though this component is rendered only on web.
 import * as IAP from 'expo-in-app-purchases'
 
 export default function InAppPurchases() {
