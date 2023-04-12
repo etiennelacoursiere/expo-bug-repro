3ba9d97016b1d4d89a89a3ad64cd821e21791a9d
Etienne Lacoursiere <lacoursiere.etienne@gmail.com>
2023-04-12 12:51:56 -0400

    init

---
+++ InAppPurchase.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Text } from 'react-native'
+import * as IAP from 'expo-in-app-purchases'
+
+export default function InAppPurchases() {
+    return (
+        <Text>IAP</Text>
+    )
+}
